c5993f7eaac4b4b94e39aded29d5321c8d89cd45
RndmNode <whitleyzeller@gmail.com>
2024-04-13 17:33:06 -0600

    Toggleable isFaceUp

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
         }
         .padding()
+        .foregroundColor(.orange)
+    }
+}
+
+struct CardView: View {
+    @State var isFaceUp = false
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp{
+                base.fill(.black)
+                base.stroke(style: /*@START_MENU_TOKEN@*/StrokeStyle()/*@END_MENU_TOKEN@*/)
+                Text("🤬").font(.largeTitle)
+            } else {
+                base
+            }
+        }.onTapGesture {
+            isFaceUp.toggle()
+        }
     }
 }
 
