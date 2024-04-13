5c47a6da8a2c1e1f84040bf106c998277ba2e015
RndmNode <whitleyzeller@gmail.com>
2024-04-13 16:41:49 -0600

    Initial Commit: Ready to start Memorize (understanding views and the functional programming basics). Added CardView and FaceUp var.

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Taylor Zeller on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
