49b95106c1a1e4fb78cebff3818b000c821ee148
RndmNode <whitleyzeller@gmail.com>
2024-04-18 17:48:33 -0600

    Memory game is playable.

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -14,6 +14,7 @@ struct EmojiMemoryGameView: View {
         VStack {
             ScrollView {
                 cards
+                    .animation(.default, value: game.cards)
             }
             Button("Shuffle"){
                 game.shuffle()
@@ -24,10 +25,13 @@ struct EmojiMemoryGameView: View {
     
     var cards: some View {
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
-            ForEach(game.cards.indices, id: \.self) { index in
-                CardView(game.cards[index])
+            ForEach(game.cards) { card in
+                CardView(card)
                     .aspectRatio(2/3, contentMode: .fit)
                     .padding(4)
+                    .onTapGesture {
+                        game.choose(card)
+                    }
             }
         }
         .foregroundColor(.orange)
@@ -56,6 +60,7 @@ struct CardView: View {
             .opacity(card.isFaceUp ? 1 : 0)
             base.fill().opacity(card.isFaceUp ? 0 : 1)
         }
+        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
     }
 }
 
--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct MemoryGame<CardContent> {
+struct MemoryGame<CardContent> where CardContent: Equatable {
     private(set) var cards: Array<Card>
     
     init(numPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
@@ -16,8 +16,8 @@ struct MemoryGame<CardContent> {
         // add number of cards * 2
         for pairIndex in 0..<max(2, numPairsOfCards) {
             let content = cardContentFactory(pairIndex)
-            cards.append(Card(content: content))
-            cards.append(Card(content: content))
+            cards.append(Card(content: content, id: "\(pairIndex + 1)a"))
+            cards.append(Card(content: content, id: "\(pairIndex + 1)b"))
         }
     }
     
@@ -25,13 +25,41 @@ struct MemoryGame<CardContent> {
         cards.shuffle()
     }
     
-    func choose(_ card: Card) {
-        
+    var indexOfTheOneAndOnlyFaceUpCard: Int? {
+        get { cards.indices.filter { index in cards[index].isFaceUp }.only }
+        set { cards.indices.forEach { cards[$0].isFaceUp = (newValue == $0) } }
     }
     
-    struct Card {
-        var isFaceUp  = true
+    mutating func choose(_ card: Card) {
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }){
+            if !cards[chosenIndex].isFaceUp && !cards[chosenIndex].isMatched {
+                if let potentialMatch = indexOfTheOneAndOnlyFaceUpCard {
+                    if cards[potentialMatch].content == cards[chosenIndex].content {
+                        cards[chosenIndex].isMatched = true
+                        cards[potentialMatch].isMatched = true
+                    }
+                } else {
+                    indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+                }
+                cards[chosenIndex].isFaceUp = true
+            }
+        }
+    }
+    
+    struct Card: Equatable, Identifiable, CustomDebugStringConvertible {
+        var isFaceUp  = false
         var isMatched = false
         let content: CardContent
+        
+        var id: String
+        var debugDescription: String {
+            return "\(id): \(content)"
+        }
+    }
+}
+
+extension Array {
+    var only: Element? {
+        count == 1 ? first : nil
     }
 }
