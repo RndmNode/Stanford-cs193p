74458ba334ec576f9f985d437130a793f0091ca1
RndmNode <whitleyzeller@gmail.com>
2024-05-11 15:48:15 -0600

    Integrating data persistence.

--- Emoji Art/Emoji Art/EmojiArt.swift
+++ Emoji Art/Emoji Art/EmojiArt.swift
@@ -7,9 +7,23 @@
 
 import Foundation
 
-struct EmojiArt {
+struct EmojiArt : Codable {
     var background: URL?
     private(set) var emojis = [Emoji]()
+    
+    init(json: Data) throws {
+        self = try JSONDecoder().decode(EmojiArt.self, from: json)
+    }
+    
+    init() {
+        
+    }
+    
+    func json() throws -> Data {
+        let encoded = try JSONEncoder().encode(self)
+        print("EmopjiArt = \(String(data: encoded, encoding: .utf8) ?? "nil")")
+        return encoded
+    }
     
     private var uniqueEmojiID = 0
     
@@ -23,13 +37,13 @@ struct EmojiArt {
         ))
     }
     
-    struct Emoji:  Identifiable{
+    struct Emoji: Identifiable, Codable {
         let string: String
         var position: Position
         var size: Int
         var id: Int
         
-        struct Position {
+        struct Position : Codable {
             var x: Int
             var y: Int
             
